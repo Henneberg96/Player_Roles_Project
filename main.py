b853858c4afdbbf05b055f251828f7d87f4d5722
mlembrecht <levi@itu.dk>
2022-09-03 09:05:37 +0200

    test commit

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import numpy as np
 
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
 def print_hi(name):
