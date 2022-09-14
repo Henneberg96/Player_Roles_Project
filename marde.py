49a0abf35a0a307eaa6bcd9938fe2d9ba7871378
mlembrecht <levi@itu.dk>
2022-09-14 16:32:36 +0200

    test commit3

--- marde.py
+++ marde.py
@@ -1,95 +1,144 @@
+import math
 import numpy as np
 import pandas as pd
+from helpers.helperFunctions import findArea
 
-#load event file
+# load event file
 df = pd.read_csv('C:/Users/mall/OneDrive - Implement/Documents/Andet/RP/Data/Wyscout_Events - Copy.csv',
-                   sep=";",
-                   encoding='unicode_escape')
+                 sep=";",
+                 encoding='unicode_escape')
 
-#drop first columns
+# drop first columns
 df.drop(columns=df.columns[0], axis=1, inplace=True)
 
-df.shape #73 columns at this point
+df.shape  # 73 columns at this point
 
-#drop columns that won't be used
+# drop columns that won't be used
 df = df.drop(['left_foot',
-                  'right_foot',
-                  'head',
-                  'take_on_left',
-                  'take_on_right',
-                  'free_space_left',
-                  'free_space_right',
-                  'high_cross',
-                  'low_cross',
-                  'clearance',
-                  'fairplay',
-                  'direct',
-                  'indirect',
-                  'low',
-                  'low_right',
-                  'center',
-                  'left',
-                  'low_left',
-                  'right',
-                  'high',
-                  'high_left',
-                  'high_right',
-                  'miss_low_right',
-                  'miss_left',
-                  'miss_low_left',
-                  'miss_right',
-                  'miss_high',
-                  'miss_high_left',
-                  'miss_high_right',
-                  'post_low_right',
-                  'post_left',
-                  'post_low_left',
-                  'post_right',
-                  'post_high',
-                  'post_high_left',
-                  'post_high_right',
-                  'blocked'],
-                 axis=1)
+              'right_foot',
+              'head',
+              'take_on_left',
+              'take_on_right',
+              'free_space_left',
+              'free_space_right',
+              'high_cross',
+              'low_cross',
+              'clearance',
+              'fairplay',
+              'direct',
+              'indirect',
+              'low',
+              'low_right',
+              'center',
+              'left',
+              'low_left',
+              'right',
+              'high',
+              'high_left',
+              'high_right',
+              'miss_low_right',
+              'miss_left',
+              'miss_low_left',
+              'miss_right',
+              'miss_high',
+              'miss_high_left',
+              'miss_high_right',
+              'post_low_right',
+              'post_left',
+              'post_low_left',
+              'post_right',
+              'post_high',
+              'post_high_left',
+              'post_high_right',
+              'blocked'],
+             axis=1)
 
-df.shape #36 columns at this point
+df.shape  # 36 columns at this point
 
-#drop irelevant sub events
+# drop irelevant sub events
 df = df[df.subEventName.notnull()]
 df = df[df.subEventName != 'Whistle']
 
-#merge with matches dataset to get season ID
+# merge with matches dataset to get season ID
 matches = pd.read_csv('C:/Users/mall/OneDrive - Implement/Documents/Andet/RP/Data/Wyscout_Matches.csv',
-                   sep=";",
-                   encoding='unicode_escape')
+                      sep=";",
+                      encoding='unicode_escape')
 df = pd.merge(df, matches, on='matchId')
 compInd = df.pop('competitionId')
 df.insert(5, 'competitionId', compInd, allow_duplicates=True)
 sznInd = df.pop('seasonId')
 df.insert(6, 'seasonId', sznInd, allow_duplicates=True)
 
-df.shape #38 columns at this point
+df.shape  # 38 columns at this point
 
-#rearranging columns
+# rearranging columns
 eIdInd = df.pop('id')
 df.insert(6, 'eventNameId', eIdInd, allow_duplicates=True)
 eNameInd = df.pop('eventName')
 df.insert(6, 'eventName', eNameInd, allow_duplicates=True)
-df.pop('eventTypeId')
+df.pop('eventId')
 subEventInd = df.pop('subEventId')
 df.insert(7, 'subEventId', subEventInd, allow_duplicates=True)
 subEventNameInd = df.pop('subEventName')
 df.insert(8, 'subEventName', subEventNameInd, allow_duplicates=True)
 
-df.shape #37 columns at this point
+df.shape  # 37 columns at this point
 
-#creating df copy for backup
-dfx = df.copy()
-
-#append df with accuracy per sub event
+# append df with accuracy per sub event
 temp = pd.unique(df['subEventName'])
 for i in temp:
     name = i + '_acc'
-    dfx[name] = np.where(dfx['subEventName'] == i, dfx['accurate'], np.nan)
+    df[name] = np.where(df['subEventName'] == i, df['accurate'], np.nan)
 
-df_p = dfx[['subEventName', 'accurate', 'Cross_acc']] #test
-dfx.shape #72 columns at this point
+df_p = df[['subEventName', 'accurate', 'Cross_acc']]  # test
+
+for i in temp:
+    name = i + '_acc'
+    if df[name].isnull().all():
+        df.drop(name, axis=1, inplace=True)
+
+df.shape  # 59 columns at this point
+
+# defining event zones
+df.insert(9, 'eventZone', df.apply(lambda row: findArea(row), axis=1), allow_duplicates=True)
+
+df.shape  # 60 columns at this point
+
+# adding shot, passing, and crossing distances
+def ec(x1, x2, y1, y2):
+    return np.sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2))
+
+
+df['shot_distance'] = np.where(df['subEventName'] == 'Shot', ec(df['x'], df['end_x'], df['y'], df['end_y']), np.nan)
+df['passing_distance'] = np.where(df['eventName'] == 'Pass', ec(df['x'], df['end_x'], df['y'], df['end_y']), np.nan)
+df['cross_distance'] = np.where(df['subEventName'] == 'Cross', ec(df['x'], df['end_x'], df['y'], df['end_y']), np.nan)
+
+df.shape #63 columns at this point
+
+#adding progressive passes
+def pp(x1, x2):
+    dist = x2 - x1
+    return np.where(x1 > 50, np.where(dist > 10, 1.00000, np.nan),
+             np.where(dist > 25, 1.00000, np.nan))
+
+
+df['progressive_passes'] = np.where(df['eventName'] == 'Pass', pp(df.x, df.end_x), np.nan)
+df['progressive_passes_distance'] = np.where(df['progressive_passes'] == 1.00000, ec(df['x'], df['end_x'], df['y'], df['end_y']), np.nan)
+
+#adding extra passes types
+def direction(x1, x2):
+    dist = x2 - x1
+    return np.where(dist > 2, 'forward',
+             np.where(dist < -2, 'backward', 'horizontal'))
+
+
+df['pass_direction'] = np.where(df['eventName'] == 'Pass', direction(df.x, df.end_x), np.nan)
+
+#adding swtiches
+def switch(y1, y2):
+    dist = y2 - y1
+    return np.where(dist > 35, 1.00000, np.nan)
+
+
+df['pass_direction'] = np.where(df['eventName'] == 'Pass', switch(df.y, df.end_y), np.nan)
+
