1349bd3a4d0d6503265563c7f9bb6ba1935413f1
Henneber96 <jesper@jash.dk>
2022-09-15 03:28:23 +0200

    code cleaned, not done

--- helpers/helperFunctions.py
+++ helpers/helperFunctions.py
@@ -8,55 +8,55 @@ def findArea(row):
     y = row['y']
     if(x >= 0 and x <= 16 and y >=0 and y <=19):
         s = '_01'
-    elif(x >= 17 and x <=33 and y >=0 and  y <=19):
+    elif(x > 16 and x <=33 and y >=0 and  y <=19):
         s = '_02'
-    elif (x >= 34 and x <= 50 and y >= 0 and y <= 19):
+    elif (x > 33 and x <= 50 and y >= 0 and y <= 19):
         s = '_03'
-    elif (x >= 51 and x <= 67 and y >= 0 and y <= 19):
+    elif (x > 50 and x <= 67 and y >= 0 and y <= 19):
         s = '_04'
-    elif (x >= 68 and x <= 84 and y >= 0 and y <= 19):
+    elif (x > 67 and x <= 84 and y >= 0 and y <= 19):
         s = '_05'
-    elif (x >= 85  and x <=100 and y >= 0 and y <= 19):
+    elif (x > 84  and x <=100 and y >= 0 and y <= 19):
         s = '_06'
-    elif (x >= 17 and x <= 33 and y >= 20 and y <= 37):
+    elif (x > 16 and x <= 33 and y > 19 and y <= 37):
         s = '_07'
-    elif (x >= 34 and x <= 50 and y >= 20 and y <= 37):
+    elif (x > 33 and x <= 50 and y > 19 and y <= 37):
         s = '_08'
-    elif (x >= 51 and x <= 67 and y >= 20 and y <= 37):
+    elif (x >= 50 and x <= 67 and y > 19 and y <= 37):
         s = '_09'
-    elif (x >= 68 and x <= 84 and y >= 20 and y <= 37):
+    elif (x > 67 and x <= 84 and y > 19 and y <= 37):
         s = '_10'
-    elif (x >= 17 and x <= 33 and y >= 38 and y <= 63):
+    elif (x > 16 and x <= 33 and y > 37 and y <= 63):
         s = '_12'
-    elif (x >= 34 and x <= 50 and y >= 38 and y <= 63):
+    elif (x > 33 and x <= 50 and y > 37 and y <= 63):
         s = '_13'
-    elif (x >= 51 and x <= 67 and y >= 38 and y <= 63):
+    elif (x > 50 and x <= 67 and y > 37 and y <= 63):
         s = '_14'
-    elif (x >= 68 and x <= 84 and y >= 38 and y <= 63):
+    elif (x > 67 and x <= 84 and y > 37 and y <= 63):
         s = '_15'
-    elif (x >= 17 and x <= 33 and y >= 64 and y <= 81):
+    elif (x > 16 and x <= 33 and y > 63 and y <= 81):
         s = '_17'
-    elif (x >= 34 and x <= 50 and y >= 64 and y <= 81):
+    elif (x > 33 and x <= 50 and y > 63 and y <= 81):
         s = '_18'
-    elif (x >= 51 and x <= 67 and y >= 64 and y <= 81):
+    elif (x > 50 and x <= 67 and y > 63 and y <= 81):
         s = '_19'
-    elif (x >= 68 and x <= 84 and y >= 64 and y <= 81):
+    elif (x > 67 and x <= 84 and y > 63 and y <= 81):
         s = '_20'
-    elif (x >= 0 and x <= 16 and y >= 81 and y <= 100):
+    elif (x >= 0 and x <= 16 and y > 81 and y <= 100):
             s = '_21'
-    elif (x >= 17 and x <= 33 and y >= 81 and y <= 100):
+    elif (x > 16 and x <= 33 and y > 81 and y <= 100):
         s = '_22'
-    elif (x >= 34 and x <= 50 and y >= 81 and y <= 100):
+    elif (x > 33 and x <= 50 and y > 81 and y <= 100):
         s = '_23'
-    elif (x >= 51 and x <= 67 and y >= 81 and y <= 100):
+    elif (x > 50 and x <= 67 and y > 81 and y <= 100):
         s = '_24'
-    elif (x >= 68 and x <= 84 and y >= 81 and y <= 100):
+    elif (x > 67 and x <= 84 and y > 81 and y <= 100):
         s = '_25'
-    elif (x >= 85 and x <= 100 and y >= 81 and y <= 100):
+    elif (x > 84 and x <= 100 and y > 81 and y <= 100):
         s = '_26'
-    elif (x >= 0 and x <= 16 and y >= 20 and y <= 80):
+    elif (x >= 0 and x <= 16 and y > 19 and y <= 81):
         s = '_11'
-    elif (x >= 84 and x <= 100 and y >= 20 and y <= 80):
+    elif (x >= 84 and x <= 100 and y > 19 and y <= 81):
         s = '_16'
     else: s = "out_of_bounce"
     return sub_e + s
---
+++ helpers/visualizations.py
@@ -0,0 +1,62 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mplsoccer import Pitch
+
+def createPitchWithZones():
+    data = np.array([
+        [0, 0],
+        [16,0], # x+16
+        [0, 19],
+        [16, 19], # x+17
+        [33, 0],
+        [33, 19],
+        [67, 0], # x+34
+        [67, 19],
+        [84, 0],
+        [84, 19],
+        [100, 0],
+        [100, 19],
+        [0, 81],
+        [0, 100],
+        [16, 81],
+        [16, 100],
+        [33, 81],
+        [33, 100],
+        [67, 81],  # x+34
+        [67, 100],
+        [84, 81],
+        [84, 100],
+        [100, 81],
+        [100, 100],
+        [16, 63],
+        [16, 63],
+        [16, 63],
+        [33, 63],
+        [67, 63],
+        [16, 37],
+        [33, 37],
+        [50, 19],
+        [50, 0],
+        [50, 37],
+        [50, 63],
+        [50, 81],
+        [50, 100],
+        [67, 37],
+        [84, 37],
+        [84, 63],])
+    pitch = Pitch(pitch_type='wyscout', axis=True,
+                  positional=True,
+                  tick=True,
+                  label=True)
+    fig, ax = pitch.draw()
+    x, y = data.T
+    plt.scatter(x,y)
+    plt.show()
+
+#Draw customized polygons
+#shape1 = np.array([[0, 0], [50, 0], [50, 50], [0, 50]])
+#shape2 = np.array([[0, 50], [50, 50], [50, 100], [0, 100]])
+#shape3 = np.array([[50, 0], [100, 0], [100, 50], [50, 50]])
+#shape4 = np.array([[50, 50], [100, 50], [100, 100], [50, 100]])
+#areas = [shape1, shape2, shape3, shape4]
+#pitch.polygon(areas , edgecolor='black', fc=[1, 0, 0], alpha=0.3, ax=ax)
